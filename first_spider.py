3d2f3d255aae53259d89947c739f8be5dc805723
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-02-28 14:18:57 +0100

    Récupération d'une liste de citations en json depuis un site

--- first_spider.py
+++ first_spider.py
@@ -1,8 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 r = requests.get('https://mohsinweb.herokuapp.com/quotes/')
 
-soup = BeautifulSoup(r.text, 'html.parser')
+soup = BeautifulSoup(r.text, 'lxml')
 
-print(soup.prettify())
+# print(soup.prettify())
+
+title = soup.h1.text
+
+quotesHTML = soup.findAll('div', {"class": "quotes"})
+
+quotes = [
+    {"quote": quote.findAll('p')[0].text.strip(),
+     "author": quote.findAll('p')[1].text.strip()}
+    for quote in quotesHTML]
+
+# Ouverture du fichier en écriture
+f = open("quote.json", "w")
+
+# Insertion au format json de la liste 'quotes'
+json.dump(quotes, f)
+
+print(quotes)
